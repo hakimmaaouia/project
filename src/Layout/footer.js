bdd0c77f566aedd5c89afd185d51443734d4e3c3
hakimmaaouia <hakimmaaouia@gmail.com>
2020-02-15 16:08:19 +0100

    footer

--- src/Layout/footer.js
+++ src/Layout/footer.js
@@ -1,9 +1,77 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import styled from "styled-components";
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import MailIcon from '@material-ui/icons/Mail';
 const Footer = () =>{
+    const Container = styled.div`
+      justify-content: center;
+      align-items: center;
+      text-align: center;
+      padding:50px 150px;
+      margin: 0;
+      background: #f1f2f6;
+      @media only screen and (max-width: 750px) {
+        padding:50px 0px;       
+}  
+`
+
+const A = styled.a`
+text-decoration:none;
+text-decoration-color:none;
+color:#2a3c76;
+:hover{
+    color:#89a0cf;
+}
+
+ `
     return(
-        <div>
-            e
-            </div>
+        <Container>
+            <Grid container   direction="row"
+  justify="center"
+  alignItems="center"
+  spacing={0} >
+      <Grid item   xs={12} sm={2}  style={{marginTop:5}} >
+          <A href="#">Home</A>
+    
+        </Grid>
+        <Grid item xs={12} sm={2} style={{marginTop:5}} >
+       <A href="#" > Get Involved</A>
+        </Grid>
+        <Grid item  xs={12} sm={2} style={{marginTop:5}} >
+        <A href="#" >Our Causes</A>
+        </Grid>
+        <Grid item  xs={12} sm={2} style={{marginTop:5}} >
+        <A href="#" >Our Programs</A>
+        </Grid>
+        <Grid item  xs={12} sm={2} style={{marginTop:5}} >
+       <A href="#" > News & Features</A>
+        </Grid>
+        <Grid item  xs={12} sm={2} style={{marginTop:5}} >
+        <A href="#" >About</A>
+        </Grid>
+
+</Grid>
+<Grid container    justify="center"
+  alignItems="center" spacing={2} style={{marginTop:20}} >
+<Grid item><A href="#" >Contact Us</A></Grid>
+<Grid item ><A href="#" >Change Language</A></Grid>
+    </Grid>     
+
+
+
+
+    <Grid container justify="center"  alignItems="center"  spacing={1} style={{marginTop:20}}>
+    <Grid item   ><A href="#" ><FacebookIcon style={{ fontSize: 35 }} /></A></Grid>
+    <Grid item  ><A href="#" ><InstagramIcon style={{ fontSize: 35 }} /></A></Grid>
+    <Grid item   ><A href="#" > <YouTubeIcon style={{ fontSize: 35 }} /></A></Grid>
+    <Grid item   ><A href="#" ><MailIcon style={{ fontSize: 35 }} /></A></Grid>
+        </Grid>      
+        <p style={{color:"#2a3c76"}}>© 2019 Rotary International. All rights reserved</p>
+            </Container>
+       
     )
 }
 export default Footer;
--- src/Layout/header.js
+++ src/Layout/header.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import styled from "styled-components";
+const Ul = styled.ul`
+background: #89a0cf;
+list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  display: flex;
+      justify-content: center;
+      align-items: center;
+      text-align: center;
+  
+`
+
+const Li = styled.li`
+ float: left;
+ a{
+    display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  :hover {
+    transition: 1s;
+  background-color: #2a3c76;
+}
+ }
+`
 const Header = () =>{
     return(
-        <div>
-            e
-            </div>
+        <Ul>
+        <Li><a class="active" href="#home"><b>Home</b></a></Li>
+        <Li><a href="#news"><b>News</b></a></Li>
+        <Li><a href="#contact"><b>Contact</b></a></Li>
+        <Li><a href="#about"><b>About</b></a></Li>
+        <Li><a href="#about"><b>Our Causes</b></a></Li>
+      </Ul>
     )
 }
 export default Header;
